a99b688a3fa6802dbb89523c919e9cbdec5256d6
KolinZard <zrd@bk.ru>
2023-11-16 22:48:51 +0400

    add .gitignore and edit README

--- Project_0_num_game/Game.py
+++ Project_0_num_game/Game.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def num_rand_game(number: int = 50) -> int:
+def num_rand_game(number: int = 1) -> int:
     """Сначала устанавливаем любое random число, а потом уменьшаем
     или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток
@@ -11,30 +11,31 @@ def num_rand_game(number: int = 50) -> int:
     Returns:
         int: Число попыток
     """
+    
     count = 0
     predict = np.random.randint(1, 101)
-    if number == predict:
+    if predict == number:
         count += 1
         return count
-    while number > predict:
+    while predict > number:
         count += 1
-        number -= 10
-        if number == predict:
+        predict -= 10
+        if predict == number:
             return count
-        while number < predict:
+        while predict < number:
             count += 1
-            number += 1
-            if number == predict:
+            predict += 1
+            if predict == number:
                 return count
-    while number < predict:
+    while predict < number:
         count += 1
-        number += 10
-        if number == predict:
+        predict += 10
+        if predict == number:
             return count
-        while number > predict:
+        while predict > number:
             count += 1
-            number -= 1
-            if number == predict:
+            predict -= 1
+            if predict == number:
                 return count
             
 
@@ -51,7 +52,7 @@ def score_game(num_rand_game) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
 
     for number in random_array:
